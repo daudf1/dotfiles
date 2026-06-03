cb612e3f4ca4685b124933e6f21c230895dad0ca
daudf1 <daudf2024@gmail.com>
2026-06-03 06:23:01 +0100

    fix setup.sh remove ollama serve

--- setup.sh
+++ setup.sh
@@ -7,10 +7,6 @@ sudo apt-get install -y zstd curl
 # Install Ollama
 curl -fsSL https://ollama.com/install.sh | sh
 
-# Start Ollama in background
-ollama serve &
-sleep 5
-
 # Pull your models
 ollama pull dolphin-mistral
 
